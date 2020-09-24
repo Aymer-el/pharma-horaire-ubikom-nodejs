117c7417d85bae8ac9d16f1bd9afe5528020ec03
PoloDelMondo <fi.aymeric@hotmail.com>
2020-09-24 17:14:05 +0200

    initialisation: initialisation MySQL Table part 1: table creation

--- database0/sql/mySQL_creationScript.sql
+++ database0/sql/mySQL_creationScript.sql
@@ -1,25 +1,31 @@
 
-USE mnhn_db1;
+USE phu_1;
 
+DROP TABLE IF EXISTS Ligne_horaire;
+DROP TABLE IF EXISTS Pharmacie;
+DROP TABLE IF EXISTS Horaire;
 
-DROP TABLE IF EXISTS user_mnhn;
-
-CREATE TABLE user_mnhn (
+CREATE TABLE Pharmacie (
   id int(11) NOT NULL AUTO_INCREMENT,
-  username varchar(50) NOT NULL,
-  email varchar(62) NOT NULL UNIQUE,
-  birthdate Date NULL,
+  pharmacieName varchar(255) NOT NULL,
+  address varchar(255) NOT NULL UNIQUE,
   PRIMARY KEY (id)
 ) DEFAULT CHARSET=utf8;
 
+CREATE TABLE Horaire (
+  horaireId smallInt NOT NULL,
+  PRIMARY KEY (horaireId)
+  
+) DEFAULT CHARSET=utf8;
 
-INSERT INTO user_mnhn (username, email) VALUES ('Jean Ventura', 'jean.ventura@gmail.com');
-INSERT INTO user_mnhn (username, email) VALUES ('Leon Blum', 'l.blum@gmail.com');
+CREATE TABLE Ligne_horaire (
+  id int(16) NOT NULL AUTO_INCREMENT,
+  horaireId smallInt NOT NULL,
+  pharmacieId int NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (horaireId) REFERENCES Horaire(horaireId),
+  FOREIGN KEY (pharmacieId) REFERENCES Pharmacie(id)
+) DEFAULT CHARSET=utf8;
 
-/*INSERT INTO user_mnhn (username, email) VALUES ('LeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeon', 'Blum&blum');*/
-/*INSERT INTO user_mnhn (username, email) VALUES ('LeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeon', 'blum@blu');*/
-/*INSERT INTO user_mnhn (username,  email) VALUES ('LeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeonLeon', 'l.bl');*/
-/* INSERT INTO user_mnhn (username, email) VALUES ('FelixViallet','l.blum@gmail.com');
-*/
 
-SELECT * FROM user_mnhn;
+SELECT * FROM Pharmacie;
